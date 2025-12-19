6214b8342dcc682ccd15168891119bd42efa24c7
tina <tina@befuture8.com>
2025-12-19 10:22:56 +0800

    使用反射移除字串頭尾空白字元

--- trim_all_strings/main.go
+++ trim_all_strings/main.go
@@ -3,9 +3,39 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"strings"
 )
 
-func TrimAllStrings(a any) {}
+func TrimAllStrings(a any) {
+	processed := make(map[uintptr]bool)
+	trim(reflect.ValueOf(a), processed)
+}
+
+func trim(v reflect.Value, processed map[uintptr]bool) {
+	switch v.Kind() {
+	case reflect.Ptr:
+		if v.IsNil() {
+			return
+		}
+		if processed[v.Pointer()] {
+			return
+		}
+		processed[v.Pointer()] = true
+
+		trim(v.Elem(), processed)
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			trim(v.Field(i), processed)
+		}
+	case reflect.String:
+		if v.CanSet() {
+			v.SetString(strings.Trim(v.String(), " "))
+		}
+	default:
+		return
+	}
+}
 
 func main() {
 	type Person struct {
