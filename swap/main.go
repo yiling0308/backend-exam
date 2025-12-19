1d5eaee02bf424ff41af4c2271270598368b4463
tina <tina@befuture8.com>
2025-12-19 09:23:57 +0800

    透過型別斷言交換a,b值

--- swap/main.go
+++ swap/main.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 )
 
-func swap[T any](a, b T) {}
+func swap[T any](a, b T) {
+	npa, ok := any(a).(*int)
+	if !ok {
+		panic("swap[T] is not an *int")
+	}
+	npb, ok := any(b).(*int)
+	if !ok {
+		panic("swap[T] is not an *int")
+	}
+
+	*npa, *npb = *npb, *npa
+}
 
 func main() {
 	a := 10
